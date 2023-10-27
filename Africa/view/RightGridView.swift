ee95f88d3ce03e533f31400f32847955c2e0c1f9
Shrey Gupta <shrey.gupta@instantsys.com>
2023-10-27 23:05:03 +0530

    Added Grid View to the Gallery View.

---
+++ Africa/view/RightGridView.swift
@@ -0,0 +1,60 @@
+//
+//  RightGridView.swift
+//  Africa
+//
+//  Created by ShreyGupta on 27/10/23.
+//
+
+import SwiftUI
+
+struct RightGridView: View {
+    
+    let animal : AnimalListItemModel
+
+    var gridColumns = [GridItem(.fixed((UIScreen.main.bounds.width) / 3), spacing: 0), GridItem(.fixed((UIScreen.main.bounds.width) * 2 / 3), spacing: 0)]
+    
+    var gridRows = Array(repeating: GridItem(.flexible(), spacing: 0), count: 2)
+    
+    var body: some View {
+    
+        LazyVGrid(columns: gridColumns, alignment: .center, content: {
+                
+            LazyHGrid(rows: gridRows, alignment: .center, content: {
+                        
+                Image(animal.image)
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width / 3, height: 100)
+                    .overlay {
+                        Rectangle()
+                            .stroke(Color.black)
+                    }
+                        
+                Image(animal.image)
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width / 3, height: 100)
+                    .overlay {
+                        Rectangle()
+                            .stroke(Color.black)
+                    }
+            })
+
+            Image(animal.image)
+                .resizable()
+                .frame(width: (UIScreen.main.bounds.width) * 2 / 3, height: 200)
+                .overlay {
+                    Rectangle()
+                        .stroke(Color.black)
+                }
+        })
+    }
+}
+
+struct RightGridView_Previews: PreviewProvider {
+    
+    static let animal : [AnimalListItemModel] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        
+        RightGridView(animal: animal[0])
+    }
+}
